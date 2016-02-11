c69246cdf76dcec60351c5523eb638fb1eef67dc
pauljdehmer <paul@lisnr.com>
2016-02-10 20:42:20 -0500

    work

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 parentdir="$(dirname "$scriptdir")"
 
 docker build -t pauljdehmer/ozwcp:latest $parentdir
-docker run --device=/dev/ttyUSB0:/dev/ttyUSB0 --net=host --name=pauljdehmer-ozwcp -d pauljdehmer/ozwcp
+docker run --device=/dev/ttyUSB0:/dev/ttyUSB0 --net=host --name=pauljdehmer-ozwcp --rm pauljdehmer/ozwcp
